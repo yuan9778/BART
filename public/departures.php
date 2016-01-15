a41610f729b1cdcbb47de8784a63420b8b93f466
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 01:28:37 -0800

    Update departures.php

--- public/departures.php
+++ public/departures.php
@@ -1,33 +1,29 @@
 <?php
-	/**
+     /**
      * departures.php
      *
      *
      * Sanqing Yuan
      * https://github.com/yuan9778/BART
      *
-	 * Outputs JSON infos about the next departures of a specified station
+     * Outputs JSON infos about the next departures of a specified station
      */
 
 
     // configuration
     require('../models/functions.php');
 
-    if (isset($_GET['station_abbr']))
-    {
+    if (isset($_GET['station_abbr'])) {
         $station = query_etd($_GET['station_abbr']);
-        if ($station === false)
-        {
+        if ($station === false) {
             trigger_error('Could not connect to BART API', E_USER_ERROR);
         }
 
         // build associative array from SimpleXML object
         $departures = [];
-        foreach ($station->etd as $etd)
-        {
+        foreach ($station->etd as $etd) {
             $departure = [];
-            foreach ($etd->estimate as $estimate)
-            {
+            foreach ($etd->estimate as $estimate) {
                 $departure['destination'] = (string) $etd->destination;
                 $departure['minutes'] = (string) $estimate->minutes;
 
@@ -37,8 +33,7 @@
 
         // sort array by each departure's minutes
         $minutes = [];
-        foreach ($departures as $key => $row)
-        {
+        foreach ($departures as $key => $row) {
             $minutes[$key] = $row['minutes'];
         }
         array_multisort($minutes, SORT_NUMERIC, $departures);
@@ -47,8 +42,7 @@
         extract(['departures' => $departures, 'station' => $station]);
         require('../views/departures.php');
     }
-    else
-    {
+    else {
         print('Could not query departures');
     }
 
